9424dd0c531d256e1aeeb85786281d9af71d0913
1xtAsh <ashraykrsingh321@gmail.com>
2025-05-06 20:44:00 +0530

    Revert "Merge tag 'LA.UM.9.14.r1-25800-LAHAINA.QSSI15.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/camera-kernel into android13-5.4-lahaina"
    This reverts commit 8efcbedd97585e46ec7b3b90b644ac1280993353, reversing
    changes made to cd0212695a766931ace06ab76fa3f24c5e02df9c.

--- techpack/camera/drivers/cam_isp/cam_isp_context.c
+++ techpack/camera/drivers/cam_isp/cam_isp_context.c
@@ -4797,10 +4797,8 @@ static int __cam_isp_ctx_config_dev_in_top_state(
 	if ((len < sizeof(struct cam_packet)) ||
 		((size_t)cmd->offset >= len - sizeof(struct cam_packet))) {
 		CAM_ERR(CAM_ISP, "invalid buff length: %zu or offset", len);
-		spin_lock_bh(&ctx->lock);
-		list_add_tail(&req->list, &ctx->free_req_list);
-		spin_unlock_bh(&ctx->lock);
-		return -EINVAL;
+		rc = -EINVAL;
+		goto free_req;
 	}
 
 	remain_len -= (size_t)cmd->offset;
